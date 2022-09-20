32934b4b9e8d756799d1c31de23942cb3a97ff13
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-09-20 18:14:12 -0500

    add task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
-import sys
-no_arg = len(sys.argv) - 1
-if no_arg == 0:
-    print("0 arguments.")
-elif no_arg == 1:
-    print("1 argument:")
-    print("1: ", sys.argv[1])
-elif no_arg > 1:
-    print("{} arguments:".format(no_arg))
-    for i in range(1, no_arg + 1):
-        print("{}:".format(i), sys.argv[i])
+if __name__ == "__main__":
+    import sys
+    no_arg = len(sys.argv) - 1
+    if no_arg == 0:
+        print("0 arguments.")
+    elif no_arg == 1:
+        print("1 argument:")
+        print("1: ", sys.argv[1])
+    elif no_arg > 1:
+        print("{} arguments:".format(no_arg))
+        for i in range(1, no_arg + 1):
+            print("{}:".format(i), sys.argv[i])
