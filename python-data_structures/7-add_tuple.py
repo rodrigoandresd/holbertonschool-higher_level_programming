42bf277ca34d6a5ed5620a2fd783145984b294e9
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-09-21 17:00:28 -0500

    add task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,5 +6,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = (tuple_b[0], 0)
     if len(tuple_b) == 0:
         tuple_b = (0, 0)
+    if len(tuple_a) == 1:
+        tuple_a = (tuple_a[0], 0)
+    if len(tuple_a) == 0:
+        tuple_a = (0, 0)
     sum = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return sum
