6105cc0ddb1f727c7ca1dc386f173289517bc4eb
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-09-21 15:42:52 -0500

    add task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            if j == i[2]:
+            if j == i[-1]:
                 print("{:d}".format(j), end="")
             else:
                 print("{:d}".format(j), end=" ")
