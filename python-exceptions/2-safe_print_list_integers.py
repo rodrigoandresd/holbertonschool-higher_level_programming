c2f80fc8500de1faca008c6c74883a53cdbfc01a
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-09-27 15:01:10 -0500

    add task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    while count < x:
+        try:
+            print('{:d}'.format(my_list[count]), end='')
+            count += 1
+        except (ValueError, TypeError):
+            count += 1
+    print()
+    return count
