d1ed69ead7170f480fad5639462ec75b667b2ba6
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-09-26 22:30:57 -0500

    add task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,13 @@
 
 
 def roman_to_int(roman_string):
-    roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if roman_string and type(roman_string) == str:
         sum = 0
         for i in roman_string:
-            sum += roman_dic.get(i)
+            if i == 'V' and roman_string[:-1] == 'I':
+                sum -= 2
+            sum += roman_d.get(i)
         return sum
     else:
         return 0
