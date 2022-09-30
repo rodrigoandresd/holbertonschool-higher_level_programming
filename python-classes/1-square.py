07f07a63041070d2757063e9bf8f07d4cd87daa8
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-09-30 07:21:58 -0500

    add task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Square class definition"""
+
+
+class Square:
+    """ Square class definition"""
+
+    def __init__(self, size):
+        """__init__ method: a square object.
+        args: size - size of the square.
+        """
+        self.__size = size
