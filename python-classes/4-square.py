1f85addc132398511f45417ae2c7c1d9a3c02c22
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-09-30 11:35:59 -0500

    add task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -16,7 +16,7 @@ class Square:
     def size(self):
         """getter for size"""
         return self.__size
-        
+
     @size.setter
     def size(self, value):
         """setter for size"""
@@ -24,7 +24,7 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-            
+
         self.__size = value
 
     def area(self):
