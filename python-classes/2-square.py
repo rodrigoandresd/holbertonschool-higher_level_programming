f2c8cac520ca9b1512d7f95d99e8933832d84bfa
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-09-30 10:58:43 -0500

    add task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" Square class definition"""
+
+
+class Square:
+    """ Square class definition"""
+
+    def __init__(self, size=0):
+        """__init__ method: a square object.
+        args: size - size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = size
+    
