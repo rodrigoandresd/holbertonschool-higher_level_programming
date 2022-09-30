c310a10d70e7773eb6ed1195603c736ebbc18e67
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-09-30 11:08:48 -0500

    add task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" Square class definition"""
+
+
+class Square:
+    """ Square class definition"""
+
+    def __init__(self, size=0):
+        """__init__ method: a square object.
+        args: size - size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """define area method, evaluate square area"""
+        return self.__size ** 2
