70abe4242ce65e9e204e613e96e658aef88c9d44
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-04 10:42:12 -0500

    task 0 check errors

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,13 +10,13 @@ def add_integer(a, b=98):
 
     Args:
         a: first integer or float
-        b: second  or float 
+        b: second  or float
     Returns:
         The return value. a + b
     """
-    if not a or not isinstance (a, (float, int)):
+    if not a or not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
-    if not b or not isinstance (b, (float, int)):
+    if not b or not isinstance(b, (float, int)):
         raise TypeError("b must be an integer")
-    
+
     return (int(a) + int(b))
