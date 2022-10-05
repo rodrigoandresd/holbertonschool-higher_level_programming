46f3891a8fadb09d28b20b50ac6ce554de9c5820
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-04 19:30:53 -0500

    correction task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -10,12 +10,12 @@ def matrix_divided(matrix, div):
      that divides all elements of a matrix
     """
     le = "matrix must be a matrix (list of lists) of integers/floats"
-    
+
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
-    
+
     if div == 0:
-            raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError("division by zero")
 
     if not matrix or not matrix[0]:
         raise TypeError(e)
