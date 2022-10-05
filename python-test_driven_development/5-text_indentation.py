c6813bffeaec5e19c7b34e5ffc9b9ec9269e5086
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-04 23:40:45 -0500

    add task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,7 +5,7 @@
 
 def text_indentation(text):
     """
-    function that prints a text with 2 new lines after each of 
+    function that prints a text with 2 new lines after each of
     these characters: ., ? and :
     """
     if type(text) is not str:
