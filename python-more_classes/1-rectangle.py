8015bda09902a17042012904920bc83fc8df429b
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-05 15:52:09 -0500

    add task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,12 +10,12 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        
+
     @property
     def width(self):
         """getter for width"""
         return self.__width
-        
+
     @width.setter
     def width(self, value):
         """setter for width"""
@@ -24,7 +24,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         """getter for height"""
