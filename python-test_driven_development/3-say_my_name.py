71751576308bae04a98c17534359524dc15610d0
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-04 21:10:24 -0500

    correction task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,8 +7,8 @@ module that contain the function say_my_name
 
 def say_my_name(first_name, last_name=""):
     """funtion that say a name"""
-    if type(first_name) not in [str]:
+    if type(first_name) is not str:
                 raise TypeError("first_name must be a string")
-    if type(last_name) not in [str]:
+    if type(last_name) is not str:
                 raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
