834819703084375886396bdb4d897b0d407fa429
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-04 23:36:01 -0500

    add task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,8 +10,10 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
+    new_str = ""
     for i in text:
+        new_str += i
         if i == '.' or i == '?' or i == ':':
-            print(i + "\n")
-        else:
-            print(i, end="")
+            print(new_str.strip() + "\n")
+            new_str = ""
+    print(new_str.strip(), end="")
