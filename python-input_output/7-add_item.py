d34496570fd5475da8a29fa1175c87ab4cb7544f
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-13 12:44:48 -0500

    7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+add_item module
+"""
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    python_list = load_from_json_file('add_item.json')
+except:
+    python_list = []
+
+for i in range(1, len(sys.argv)):
+    python_list.append(sys.argv[i])
+
+save_to_json_file(python_list, 'add_item.json')
