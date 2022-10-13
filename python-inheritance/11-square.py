37a4f4a13de1aa901b5c908b33cc2a3a34acb0e2
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-12 22:00:04 -0500

    11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size):
+        """__init__ constructor method"""
+        self.integer_validator("size", size)
+        self.__size = size
+        self._Rectangle__width = size
+        self._Rectangle__height = size
+
+    def area(self):
+        """Area method"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """prints a string representation of the Square instance"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
