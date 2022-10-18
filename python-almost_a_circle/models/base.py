59905cd34e6f7bc4d0388990d9464b8fd46bcbad
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-18 15:05:50 -0500

    1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+First clase Base
+"""
+
+
+from multiprocessing.sharedctypes import Value
+
+
+class Base:
+    """
+    this clase base of this proyect
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """class constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+
