67249e973a29d1d40a2c5e4c7d75dd0c0373199f
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-18 23:42:33 -0500

    3

---
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" test for the rectangle class"""
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+class Test_Rectangle(unittest.TestCase):
+
