f57ebcfb10cb1cc539323f1ec75eb3627052a9c1
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-19 15:23:41 -0500

    15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,6 +6,7 @@ First clase Base
 
 
 from multiprocessing.sharedctypes import Value
+import json
 
 
 class Base:
@@ -21,3 +22,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == []: 
+            return "[]"
+        return json.dumps(list_dictionaries)
