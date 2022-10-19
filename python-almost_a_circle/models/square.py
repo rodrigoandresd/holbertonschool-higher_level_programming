40baa9dc76364106fd970f49a4524a7b86abaaf1
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-19 11:48:55 -0500

    10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""square class"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                       self.x, self.y,
+                                                            self.width)
