26ca133a9ec1b8579ddd5eafb00819e03097555e
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-19 15:32:05 -0500

    15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,7 +26,7 @@ class Base:
         """
         Method that returns the list of the JSON string representation
         """
-        if list_dictionaries is None or list_dictionaries == []: 
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
 
         return json.dumps(list_dictionaries)
