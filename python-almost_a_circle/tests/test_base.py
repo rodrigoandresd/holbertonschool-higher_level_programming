c8032fda183423494af79c178ea8889006987d50
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-20 00:00:56 -0500

    update del test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -5,6 +5,7 @@
 import unittest
 from models.base import Base
 
+
 class TestBase(unittest.TestCase):
     """class test"""
     def test_auto_id(self):
@@ -16,5 +17,17 @@ class TestBase(unittest.TestCase):
     def test_auto_id_saved(self):
         b2 = Base(89)
         self.assertEqual(b2.id, 89)
+
+    def test_to_json_str_none(self):
+        self.assertEqual(Base.to_json_string(None), '[]')
+
+    def test_to_json_str_empty(self):
+        self.assertEqual(Base.to_json_string([]), '[]')
+
+    def test_to_json_str_id(self):
+        self.assertEqual(Base.to_json_string([ { 'id': 12 }]), '[{"id": 12}]')
+
+    def test_to_json_str_id_exist(self):
+        self.assertEqual(Base.to_json_string([ { 'id': 12 }]), '[{"id": 12}]')
 if __name__ == '__main__':
     unittest.main()
