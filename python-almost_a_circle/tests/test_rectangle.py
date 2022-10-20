063a06c6cadae67b89799d2d538e0ad9954b6b2f
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-20 13:25:18 -0500

    test assertin

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -19,10 +19,29 @@ class Test_rectangle(unittest.TestCase):
         self.assertRaises(TypeError, Rectangle, "10", 2)
         self.assertRaises(TypeError, Rectangle, 10, 8, "3", 5)
         self.assertRaises(TypeError, Rectangle, 10, 8, 3, "5")
-        self.assertRaises(TypeError, Rectangle, 1, 2, 3, 4, 5, 6, 7)
         self.assertRaises(ValueError, Rectangle, 1, -2)
         self.assertRaises(ValueError, Rectangle, -1, 2)
         self.assertRaises(ValueError, Rectangle, 0, 2)
         self.assertRaises(ValueError, Rectangle, 1, 0)
         self.assertRaises(ValueError, Rectangle, 10, 8, 0, -2)
         self.assertRaises(ValueError, Rectangle, 10, 8, -9, 2)
+
+    def test_area(self):
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.area(), 20)
+
+    def test_class_method_presence(self):
+        """Test that the Rectangle methods are all present"""
+        l1 = dir(Rectangle)
+        self.assertIn('__init__', l1)
+        self.assertIn('width', l1)
+        self.assertIn('height', l1)
+        self.assertIn('x', l1)
+        self.assertIn('y', l1)
+        self.assertIn('area', l1)
+        self.assertIn('display', l1)
+        self.assertIn('__str__', l1)
+        self.assertIn('update', l1)
+        self.assertIn('to_dictionary', l1)
+        self.assertIn('from_json_string', l1)
+        self.assertIn('load_from_file', l1)
