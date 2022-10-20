0f0e9a612370c216c2f940876c80bc2d3221beed
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-20 17:23:55 -0500

    square test

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -9,6 +9,11 @@ from models.square import Square
 class Test_square(unittest.TestCase):
     """Class Test for square models"""
 
+    def test_square_creation_1(self):
+        rect = Square(1, 2)
+        self.assertEqual(rect.size, 1)
+        self.assertEqual(rect.x, 2)
+
     def test_type_value(self):
         self.assertRaises(TypeError, Square, "2")
         self.assertRaises(TypeError, Square, None)
@@ -17,7 +22,6 @@ class Test_square(unittest.TestCase):
         self.assertRaises(TypeError, Square, (2, 5))
         self.assertRaises(TypeError, Square, 10, 8, "3")
         self.assertRaises(TypeError, Square, 10, 3.5, 3)
-        self.assertRaises(TypeError, Square, 1, 2, 3, 4, 5, 6)
         self.assertRaises(TypeError, Square)
         self.assertRaises(ValueError, Square, 0)
         self.assertRaises(ValueError, Square, -10)
