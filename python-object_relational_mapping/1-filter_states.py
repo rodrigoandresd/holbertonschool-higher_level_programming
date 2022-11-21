8f8dda39e7e91217dcce4de94917287da350dadd
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-11-21 15:33:14 -0500

    correction variable

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,15 +9,15 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    conn = MySQLdb.connect(host="localhost",
+    db = MySQLdb.connect(host="localhost",
                            user=sys.argv[1],
                            passwd=sys.argv[2],
                            db=sys.argv[3])
-    cur = conn.cursor()
+    cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name\
     LIKE BINARY 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
     cur.close()
-    conn.close()
+    db.close()
--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,15 +8,15 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    conn = MySQLdb.connect(host="localhost",
+    db = MySQLdb.connect(host="localhost",
                            user=sys.argv[1],
                            passwd=sys.argv[2],
                            db=sys.argv[3])
-    cur = conn.cursor()
+    cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC",
                 (sys.argv[4],))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
     cur.close()
-    conn.close()
+    db.close()
