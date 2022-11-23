7469d8ecafb6f0011ecb4a3c21505741f1b7706f
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-11-23 12:42:21 -0500

    better

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 
 def best_score(a_dictionary):
     if a_dictionary:
-        max_key = max(a_dictionary, key=a_dictionary.get)
+        max_key = max(a_dictionary)
         return max_key
     else:
         return None
