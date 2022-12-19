4c9adcdb0b4ee083f44204af6047313d9056815a
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-19 10:59:56 -0500

    task 9. add

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// scrip that prints the addition of 2 integers
+
+const argument = process.argv.slice(2);
+
+function add(a, b) {
+    return a + b;
+}
+const x = parseInt(argument[0]);
+const y = parseInt(argument[1]);
+
+console.log(add(x, y));
