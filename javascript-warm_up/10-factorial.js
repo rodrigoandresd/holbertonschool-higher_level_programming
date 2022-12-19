12838cd9272859bbf4535154ae6744a8d168a55a
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-19 11:46:15 -0500

    task 10 factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// scrip that computes and prints a factorial
+
+const argument = process.argv.slice(2);
+const firstArgument = parseInt(argument[0]);
+function factorial (num) {
+	if (num <= 1) {
+		return 1;
+	}
+	return num * factorial(num - 1);
+} if (isNaN(firstArgument)) {
+	console.log(factorial(1));
+} else {
+	console.log(factorial(firstArgument));
+}
