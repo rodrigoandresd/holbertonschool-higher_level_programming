2783ae8f57fd9f3621c2846780df61f2c1a55556
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-19 16:33:27 -0500

    task 1- Rectangle #  semistandard validation

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -2,7 +2,7 @@
 // empty class Rectangle that defines a rectangle
 
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
