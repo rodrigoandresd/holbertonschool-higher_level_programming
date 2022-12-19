d89bab3d9885a2bce4235d69c1ff0c881ec3fa37
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-19 09:13:21 -0500

    task 5. An Integer

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* script that prints My number: if the first argument can be
+converted to an integer */
+const argument = process.argv.slice(2)
+if (isNaN(argument[0]) || argument.length == 0) {
+    console.log('Not a number');
+} else {
+    console.log('My number: ' + parseInt(argument[0]));
+}
