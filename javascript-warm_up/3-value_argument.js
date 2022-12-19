2b340d795f4a10dc630ef46bb60b4ee12e295cf3
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-19 08:51:17 -0500

    task 3. Value of my argument -corrections

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -3,7 +3,7 @@
 const argument = process.argv.slice(2);
 
 if (argument[0]) {
-    console.log(argument[0]);
+  console.log(argument[0]);
 } else {
-    console.log('No argument');
+  console.log('No argument');
 }
