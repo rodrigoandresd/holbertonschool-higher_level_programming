eafc884669ca453b43fd58258d475b3c18df01c9
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-19 14:44:58 -0500

    task 11. second biggest

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// scrip that searches the second biggest integer in the list of arguments.
+
+const argument = process.argv.slice(2);
+
+if (argument.length == 0 || argument.length == 1) {
+  console.log('0')
+} else {
+  const theBiggest = Math.max.apply(null, argument);
+  newArray = [];
+  for (let i = 0; i < argument.length; i++)
+    if (parseInt(argument[i]) != theBiggest) {
+      newArray.push(argument[i]);
+  }
+  console.log(Math.max.apply(null, newArray));
+}
