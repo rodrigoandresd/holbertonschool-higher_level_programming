58d3de28a84ad445dd9dbbfc6c2c350dc43c474c
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-19 08:47:19 -0500

    task 3. Value of my argument

---
+++ javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script that prints the first argument passed to it
+const argument = process.argv.slice(2);
+
+if (argument[0]) {
+    console.log(argument[0]);
+} else {
+    console.log('No argument')
+}
