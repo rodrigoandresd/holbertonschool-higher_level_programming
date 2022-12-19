ab8cffb3822228ced7d7dbfde1631aa08cfab468
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-19 10:45:34 -0500

    task 8. square correction semistandard validation

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,11 +2,10 @@
 // scrip that print a square
 const argument = process.argv.slice(2);
 const sizeOfSquare = parseInt(argument[0]);
-const character = 'X'
+const character = 'X';
 if (isNaN(argument[0])) {
   console.log('Missing size');
 }
 for (let i = 0; i < sizeOfSquare; i++) {
   console.log(character.repeat(sizeOfSquare));
 }
-
