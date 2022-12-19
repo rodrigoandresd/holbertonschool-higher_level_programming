4f776b87b6bd3466473a510659f5e81326b5db54
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-19 15:29:05 -0500

    task 2- corrections semistandard validation

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 // script that prints a message depending of the number of arguments passed
 const argument = process.argv.slice(2);
 
-if (argument.length == 0) {
-    console.log('No argument');
-} else if (argument.length == 1) {
-    console.log('Argument found');
+if (argument.length === 0) {
+  console.log('No argument');
+} else if (argument.length === 1) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
