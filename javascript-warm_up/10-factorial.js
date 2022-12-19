4e96143cc7a0b49393b14bde1fec6eaad4bcec55
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-19 11:52:22 -0500

    task 10 factorial correction sv

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -4,10 +4,10 @@
 const argument = process.argv.slice(2);
 const firstArgument = parseInt(argument[0]);
 function factorial (num) {
-	if (num <= 1) return 1;
-	return num * factorial(num - 1);
+  if (num <= 1) return 1;
+  return num * factorial(num - 1);
 } if (isNaN(firstArgument)) {
-	console.log(factorial(1));
+  console.log(factorial(1));
 } else {
-	console.log(factorial(firstArgument));
+  console.log(factorial(firstArgument));
 }
