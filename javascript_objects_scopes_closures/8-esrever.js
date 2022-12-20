83922531aa2cc58464c3829c247370273eb6dcdf
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-19 21:10:46 -0500

    task 8. Esrever variable definition.2

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 // function that returns the reversed version of a list
 
 exports.esrever = function (list) {
-  let newList = [];
+  const newList = [];
   for (let i = list.length - 1; i >= 0; i--) {
     newList.push(list[i]);
   }
