f1c2ccd12f7903ec6360d2cb5968450eeced6aa0
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-20 11:34:21 -0500

    task 0 read file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file.
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', (error, content) => {
+  if (error) console.log(error);
+  else console.log(content);
+});
