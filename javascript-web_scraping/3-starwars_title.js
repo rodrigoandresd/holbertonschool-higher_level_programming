c0a2f74eec0a422c70bc11e54f6b92a5e93d7505
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-20 16:14:51 -0500

    task 3.  correction sv

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-  /* script that prints the title of a Star Wars movie where the episode
-  number matches a given integer.*/
+// script that prints the title of a Star Wars movie
 
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
