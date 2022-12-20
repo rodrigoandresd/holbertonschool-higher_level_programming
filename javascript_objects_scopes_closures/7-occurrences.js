5667234c6530893ca7884b61b943d7d10828bccc
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-19 20:55:39 -0500

    task 7. occurrences

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// function that returns the number of occurrences in a list
+
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      counter += 1;
+    }
+  }
+  return counter;
+};
